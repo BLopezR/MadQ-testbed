fbcbb144799a73c4795e5e642e00399cabd57084
unknown <blanca.lopez@imdea.org>
2025-02-03 17:48:21 +0100

    Bug fixes

--- Qd2/docker_image/kme_qd2.py
+++ Qd2/docker_image/kme_qd2.py
@@ -18,11 +18,11 @@ mv_ip = "10.4.32."
 # Clave: IP de la MV del KME. Valores: IP del qnode, nombre de la app, ip del KME anterior y la IP del
 # KME en el siguiente salto
 dic_ips = {
-    "10.4.32.221": ["10.4.32.207", "app1@testbed-qd2node-1", "10.4.32.96"],
-    "10.4.32.96": ["10.4.32.121", "app2@testbed-qd2node-2", "10.4.32.221", "10.4.32.144"],
-    "10.4.32.144": ["10.4.32.106", "app3@testbed-qd2node-3", "10.4.32.96", "10.4.32.227"],
-    "10.4.32.227": ["10.4.32.124", "app4@testbed-qd2node-4", "10.4.32.144", "10.4.32.236"],
-    "10.4.32.236": ["10.4.32.107", "app5@testbed-qd2node-5", "10.4.32.227"]
+    "10.4.32.221": ["10.4.32.207", "app1@qnode1", "10.4.32.96"],
+    "10.4.32.96": ["10.4.32.121", "app2@qnode2", "10.4.32.221", "10.4.32.144"],
+    "10.4.32.144": ["10.4.32.107", "app5@qnode5", "10.4.32.96", "10.4.32.227"],
+    "10.4.32.227": ["10.4.32.153", "app7@qnode7", "10.4.32.144", "10.4.32.236"],
+    "10.4.32.236": ["10.4.32.252", "app6@qnode6", "10.4.32.227"]
 }
 
 qclient.connect(dic_ips[mv_ip][0])
@@ -33,8 +33,6 @@ received_ete = threading.Event()
 
 def ksid_socket():
     global prev_ksid
-    global size
-    global dst
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(("0.0.0.0", 65431))
@@ -46,10 +44,7 @@ def ksid_socket():
                 recibiendo = True
                 while recibiendo:
                     data = conn.recv(1024)
-                    espacio = " ".encode()
-                    prev_ksid, dst_bytes, size_bytes = data.split(espacio)
-                    dst = dst_bytes.decode()
-                    size = int(size_bytes.decode())
+                    prev_ksid = data
                     recibiendo = False
                 new_ksid.set()
 
@@ -65,13 +60,14 @@ def key_socket():
                 print(f"Connected by {addr}")
                 recibiendo = True
                 while recibiendo:
-                    end_to_end_encrypted = conn.recv(1024).decode()
+                    end_to_end_encrypted = conn.recv(1024).hex()
                     recibiendo = False
                 received_ete.set()
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         global end_to_end
+        global end_to_end_encrypted
 
         parsed_path = urlparse(self.path) #In theory, the url request must follow the ETSI 014 standard, an example would be: http://KME_IP/api/v1/keys/APP_ID/enc_keys?src=SRC_kme_name&dst=DST_kme_name&size=16
         path_list = parsed_path[2].split("/")
@@ -83,21 +79,18 @@ class MyHandler(BaseHTTPRequestHandler):
         size = int(request_param[2].split("=")[1])
         print(f"Request from app {app_ID}. Request soruce: {src}, request destination: {dst}, key size: {size}")
 
-        if src == ip: #Case in which the application made the request to the "first" KME 
+        if src == mv_ip: #Case in which the application made the request to the "first" KME
             end_to_end = secrets.token_bytes(size) #Initial KME has to create the end-to-end key
             next_kme_ip = dic_ips[mv_ip][2]
 
             #Start key stream with the next KME
-            response = qclient.open_connect("app1@qd2node1", "app2@qd2node2", key_chunk_size=size, ttl=100000, ksid=None)
+            response = qclient.open_connect(dic_ips[mv_ip][1], dic_ips[next_kme_ip][1], key_chunk_size=size, ttl=100000, ksid=None)
             ksid1 = response["ksid"]
             print("Key stream response with next kme: ", response)
             #Send the key stream ID to the next KME
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.connect((next_kme_ip, 30001))
-                dst_bytes = dst.encode()
-                size_bytes = str(size).encode()
-                espacio = " ".encode()
-                data = ksid1+espacio+dst_bytes+espacio+size_bytes
+                data = ksid1
                 s.sendall(data)
             time.sleep(2)
             #Retrieve quantum key from the key stream
@@ -111,12 +104,13 @@ class MyHandler(BaseHTTPRequestHandler):
             end_to_end_encrypted = onetimepad.encrypt(end_to_end.hex(), key.hex())
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.connect((next_kme_ip, 30003))
-                s.sendall(end_to_end_encrypted.encode())
+                s.sendall(bytes.fromhex(end_to_end_encrypted))
 
-        elif dst==ip: #Case in which the application made the request to the "last" KME
+        elif dst == mv_ip: #Case in which the application made the request to the "last" KME
             #Wait for a ksid
             new_ksid.wait()
-            response = qclient.open_connect("app5@qd2node5", "app6@qd2node6", key_chunk_size=size, ttl=100000, ksid=prev_ksid)
+            prev_kme_ip = dic_ips[mv_ip][2]
+            response = qclient.open_connect(dic_ips[prev_kme_ip][1], dic_ips[mv_ip][1], key_chunk_size=size, ttl=100000, ksid=prev_ksid)
             print("Key stream response with previous kme: ", response)
             while True:
                 response = qclient.get_key(prev_ksid)
@@ -125,13 +119,15 @@ class MyHandler(BaseHTTPRequestHandler):
                     break
             print("Retireved quantum key: ", key)
             #Wait for the encrypted end-to-end key
-            received_ete.wait() 
+            received_ete.wait()
             end_to_end = bytes.fromhex(onetimepad.decrypt(end_to_end_encrypted, key.hex()))
-        
+
             #Send response to the application
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
+        response_content = f"Key: {end_to_end}"
+        self.wfile.write(response_content.encode())
 
 def forwarding_ete():
     global end_to_end_encrypted
@@ -148,9 +144,9 @@ def forwarding_ete():
                 key1 = response["key_buffer"]
                 break
         print("Retireved quantum key: ", key1)
-        
+
         # Receive encrypted end to end key from previous kme
-        received_ete.wait() 
+        received_ete.wait()
         end_to_end = bytes.fromhex(onetimepad.decrypt(end_to_end_encrypted, key1.hex()))
         next_kme = dic_ips[mv_ip][3]
         # Connect with next qnode
@@ -160,10 +156,7 @@ def forwarding_ete():
         #Send the key stream ID to the next KME
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((next_kme, 30001))
-            dst_bytes = dst.encode()
-            size_bytes = str(size).encode()
-            espacio = " ".encode()
-            data = ksid2+espacio+dst_bytes+espacio+size_bytes
+            data = ksid2
             s.sendall(data)
         time.sleep(2)
         #Retrieve quantum key (common with next kme) from the key stream
@@ -175,20 +168,18 @@ def forwarding_ete():
         print("Retireved quantum key: ", key2)
         #Encrypt end-to-end key using OTP and send it to the next KME
         end_to_end_encrypted = onetimepad.encrypt(end_to_end.hex(), key2.hex())
-        if change_path.is_set():
-            break
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((next_kme, 30003))
-            s.sendall(end_to_end_encrypted.encode())
+            s.sendall(bytes.fromhex(end_to_end_encrypted))
 
 if __name__=="__main__":
 
     ksid_socket_thread = threading.Thread(target=ksid_socket)
-    key_socket_thread = threading.Thread(target=key_socket)    
+    key_socket_thread = threading.Thread(target=key_socket)
 
     ksid_socket_thread.start()
     key_socket_thread.start()
-    
+
 
     if kme_name=="kme-1" or kme_name=="kme-5": # if kme is an end one
         http_host = ip
@@ -204,7 +195,7 @@ if __name__=="__main__":
 
         webServer.server_close()
         print("Server stopped")
-    
+
     else:
         fwd_thread = threading.Thread(target=forwarding_ete)
         fwd_thread.start()
