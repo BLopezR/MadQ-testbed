1dd5e5e938d216d4324ce614a819d7e7c6884704
adbricio <angela.diaz@imdea.org>
2025-02-04 17:28:58 +0100

    added kme actual ips and fixed bugs

--- pQKD/kme_pqkd.py
+++ pQKD/kme_pqkd.py
@@ -1,5 +1,4 @@
 import requests
-import json
 from flask import Flask, request, jsonify
 
 # Hay que instalar flask, requests
@@ -7,10 +6,9 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Mapping of KME IPs to corresponding pQKD IPs
-# TODO add actual KME IPspython3 
 KME_TO_DEVICE_MAP = {
-    "127.0.0.1": "10.4.32.42",  # Alice
-    "": "10.4.32.43"            # Bob
+    "10.4.33.41": "10.4.32.42",  # Alice, KME 1
+    "10.4.33.75": "10.4.32.43"   # Bob, KME extra
 }
 
 # Server configuration
@@ -28,7 +26,7 @@ def forward_request(full_path):
 
     # Get the IP of the KME (the server's own IP)
     kme_ip = request.host.split(":")[0]  # Extract IP from the request URL
-    print(f"Received request at KME {kme_ip} for path: {full_path}")
+    print(f"Received request at KME {kme_ip} for path: {full_path} with params: {request.args}")
 
     # Check if the KME IP is recognized
     if kme_ip not in KME_TO_DEVICE_MAP:
@@ -36,9 +34,11 @@ def forward_request(full_path):
 
     # Get the pQKD's IP
     pqkd_ip = KME_TO_DEVICE_MAP[kme_ip]
-    print(pqkd_ip)
-    target_url = f"http://{pqkd_ip}:8082/api/v1/keys/{full_path}"
-    print(target_url)
+    base_url = f"http://{pqkd_ip}:8082/api/v1/keys/{full_path}"
+    query_string = "&".join([f"{key}={value}" for key, value in request.args.items()])
+    target_url = f"{base_url}?{query_string}" if query_string else base_url  # Only add "?" if there are parameters
+
+    print(f"Forwarding request to: {target_url}")  # Debugging output
 
     try:
         # Forward the request with all query parameters
